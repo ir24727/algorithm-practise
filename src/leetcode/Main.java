d582dd163cf80043879258a6c87c8196b3b5ac3d
WEI LIU <13826277973@163.com>
2025-08-24 15:27:30 +0100

    two search parctice

---
+++ src/leetcode/Main.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        TwoSearch ts = new TwoSearch();
+        int idx = ts.search(nums, target);
+
+        System.out.println(idx); // 期望输出 4
+    }
+}
