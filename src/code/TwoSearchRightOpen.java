6af7e53accbcbdb072f419ae3af0cff606c3d924
WEI LIU <13826277973@163.com>
2025-08-24 15:48:01 +0100

    two search upgrade

---
+++ src/code/TwoSearchRightOpen.java
@@ -0,0 +1,22 @@
+package code;
+
+public class TwoSearchRightOpen {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while(left < right) {
+            int middle = left + ((right-left)/2); //防止溢出
+            if(nums[middle] > target){
+                right = middle;
+            }
+            else if(nums[middle] < target) {
+                left = middle+1;
+            }
+            else{
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
